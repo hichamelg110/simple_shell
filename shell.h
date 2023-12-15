812be801162a3fb28fa132c5177c4c16f0d7e7c5
hichamelg110 <elisejuneaileen@gmail.com>
2023-12-15 09:10:17 -0800

    simple_shell_project

--- shell.h
+++ shell.h
@@ -15,9 +15,9 @@
  * The used prototype
  */
 void hiczak_prompt_displaying(void);
-void hiczak_print(const char *message)
+void hiczak_print(const char *message);
 void error_handler(const char* error_message);
-void command_user_enter(char *command, size_t s)
+void command_user_enter(char *command, size_t s);
 void command_running(const char *command);
 
 #endif
