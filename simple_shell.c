47e6e1db563b5052d3aaca75ebfa3945ec64a21f
hichamelg110 <elisejuneaileen@gmail.com>
2023-12-15 10:06:34 -0800

    simple_shell_project

--- simple_shell.c
+++ simple_shell.c
@@ -5,17 +5,15 @@
  * for input, runs the command, and then frees the allocated memory.
  * Return: 0 if successful, otherwise returns error
  */
-
 int main(void)
 {
-char* memory_comm = malloc(230);
+char command[230];
+
 while (1)
 {
-hiczak_prompt_displaying();
-command_user_enter(memory_comm, 230);
-command_running(memory_comm);
+display_prompt();
+read_command(command, sizeof(command));
+execute_command(command);
 }
-free(memory_comm);
-
-return (0);
+return 0;
 }
