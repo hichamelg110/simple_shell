88f4cc5df72af25c77851d72bdabcd2c563a92f7
el-brown <zakariaayoub366@gmail.com>
2023-12-15 17:17:38 -0800

    adlf'fk

--- simple_shell.c
+++ simple_shell.c
@@ -1,9 +1,6 @@
 #include "shell.h"
 /**
  * main - Entry point.
- * a function that allocates memory for a command,
- * continuously prompts the user
- * for input, runs the command, and then frees the allocated memory.
  * Return: 0 if successful, otherwise returns error
  */
 
