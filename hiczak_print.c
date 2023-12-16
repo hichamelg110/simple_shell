cef68d7f112008c36678df0c873b4bbe86a6dd73
el-brown <zakariaayoub366@gmail.com>
2023-12-15 17:59:53 -0800

    del

--- hiczak_print.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-/**
- * hiczak_print - a function that writes
- * @output: string to be written.
- */
-void hiczak_print(const char *output)
-{
-write(STDOUT_FILENO, output, strlen(output));
-}
