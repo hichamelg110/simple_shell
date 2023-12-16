f3b11ef082b9331d3f8e11e4f3b38ea7d5abd65e
el-brown <zakariaayoub366@gmail.com>
2023-12-15 18:06:28 -0800

    nvnjb

--- shell.h
+++ shell.h
@@ -9,32 +9,9 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 
-/* Function that displays the shell prompt. */
 void hiczak_prompt_displaying(void);
-
-/*
- * Function that prints a message to the standard output.
- * message: The message to be printed.
- */
 void hiczak_print(const char *message);
-
-/*
- * Function that reads a command from the user.
- * command: Buffer to store the entered command.
- * s: Size of the buffer.
- */
 void command_user_enter(char *command, size_t s);
-
-/*
- * Function that runs a command.
- * command: The command to be executed.
- */
 void command_running(const char *command);
-
-/*
- * function that prints an error message and exits the program.
- */
-
 void error_handler(const char *error_msg);
-#endif /* SHELL_H */
-
+#endif
