fcaabd4b45a4675bb514f22722804df131cecde1
el-brown <zakariaayoub366@gmail.com>
2023-12-15 17:10:49 -0800

    hi man

--- hiczak_prompt.c
+++ hiczak_prompt.c
@@ -1,9 +1,7 @@
 #include "shell.h"
 /**
  * hiczak_prompt_displaying - Function that
- * displays a prompt to the standard output
  */
-
 void hiczak_prompt_displaying(void)
 {
 hiczak_print("hic_zak$");
